7336820b1f49e1eeb1d177d6b5b868dbcc509b60
flashfan88 <lavarjohnson20@gmail.com>
2019-08-30 14:31:13 -0500

    itired

---
+++ project.py
@@ -0,0 +1,91 @@
+## Problem 1:
+# Ask a user for the year they were born by calculating their age.
+# Assuming they already had their birthday and it’s 2019 print “You are [AGE] years old”
+
+# userage =
+# userInput = input("what year were your born?")
+# if userInput
+
+
+
+# Problem 2:
+# Ask the user for a string.
+# If they enter “Kenn”, “Kevin”, “Erin”, or “Autumn” print “Hello Teacher”.
+# Otherwise print “Hello Student”
+
+# userInput = input("Enter some peoples names")
+# if userInput == "Kevin":
+#     print("Hello Teacher")
+# else:
+#     print("Hello Student")
+
+
+
+# Problem 3:
+# Ask the user for a negative number.
+# Print from 7 down to the user's negative number.
+# You must include the user's number.
+
+# userInput = int(input("Enter a negative number"))
+# for userInput in range(7, userInput - 1, -1):
+#     print(userInput)
+
+
+# Problem 4:
+# Ask the user to enter a number between -10 to -5.
+# If their input is not between the two numbers ask them to do it
+# again until they get it right. Afterward they print the correct number, print "Good job"
+
+# userInput = ""
+# while userInput != -10:
+#     userInput = int(input("Enter a number between -10 to -5"))
+
+# print("Good Job")
+
+# Problem 5:
+# Create the list: squad = ["One", "Two", "Three", "Four", "Five"].
+# Print the list in reverse without using a list method.
+
+
+
+
+# Problem 6:
+# Create a function that will have the string firstName as a parameter.
+# In the function ask the user for their last name.
+# Print "Hello [FIRST & LAST NAME]" in the function. Call that function
+
+def name(firstName):
+    firstName = LaVar
+    userInput = input("Whats is your Last name")
+    print(f'Hello {firstName + userInput}')
+
+name(firstName)
+# Problem 7:
+# Create the class Books with name, rating, genre, and author properties/attributes.
+# Create a class method that will change the rating of the book.
+# Outside of the class, create three objects of the class Book and put them in an array.
+# Lastly, USING THE ARRAY print only the names of the books using any method we’ve learned in class.
+
+
+
+
+# Problem 8:
+# Create a function that asks the user to enter a number.
+# If the number is between and include -50 and 5, return "Funds too low".
+# If the number is between 5 and 50, return “You should add more funds.”
+# If the number is more than 50, return “Just enough.”
+
+
+
+# Problem 9:
+# Ask the user for a positive number.
+# Create an empty array and starting from zero, add each number by 1 into the array.
+# Print EACH ELEMENT of the array.
+
+
+
+# Problem 10:
+# Create a new empty array called pet_list.
+# Create a Pet class with a type and breed attribute/property.
+# Include a method that will print each attribute/property.
+# Add 3 pet objects to the pet_list array. Print the attributes/properties for each pet.
